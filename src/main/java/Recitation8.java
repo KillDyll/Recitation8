25c19ba2a54afe0e6d6f9282c3b8abc4661cbefd
dylandixon <djdixon22@students.desu.edu>
2023-11-22 23:44:32 -0500

    Recitation8

--- src/main/java/Recitation8.java
+++ src/main/java/Recitation8.java
@@ -3,11 +3,36 @@ public class Recitation8 {
      * You may add any private helper method you see necessary,
      * but you may NOT change the signatures of the methods below.
      */
-    public static boolean lastItemReachable(int[] hopList){
-        return false; // replace this line with your code
+    public static boolean lastItemReachable(int[] hopList) {
+        int max = 0;
+
+        for (int i = 0; i < hopList.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + hopList[i]);
+            if (max >= hopList.length - 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static int[] slidingWindowMin(int[] numberList, int windowSize){
-        return new int[0]; // replace this line with your code
+    public static int[] slidingWindowMin(int[] numberList, int windowSize) {
+        int n = numberList.length;
+        int[] result = new int[n - windowSize + 1];
+        int resultIndex = 0;
+
+        for (int i = 0; i <= n - windowSize; i++) {
+            int min = numberList[i];
+            for (int j = 1; j < windowSize; j++) {
+                if (numberList[i + j] < min) {
+                    min = numberList[i + j];
+                }
+            }
+            result[resultIndex++] = min;
+        }
+        return result;
     }
 }
+
